f8e7a6a3ba3b226373aa63a6e303203af8b941c9
kuangjian <2910508410@qq.com>
2022-12-03 13:42:02 +0800

    init

---
+++ src/Databases.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * happy coding!!!
+ */
+namespace TinyDB;
+
+class Databases
+{
+    /**
+     * @var Table{"id":string}
+     */
+    protected array $tables;
+}
---
+++ src/Table.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * happy coding!!!
+ */
+namespace TinyDB;
+
+class Table
+{
+    protected Engine $engine;
+}
